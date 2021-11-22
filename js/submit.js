fc5e3db63a41d9449886fb385426b9af56a8e5d1
ayelenleclerc <ayelenleclerc@gmail.com>
2021-11-22 12:35:06 -0300

    terminado

--- js/submit.js
+++ js/submit.js
@@ -1,13 +1,14 @@
 const $form = document.querySelector('#form');
-const $buttonMailto = document.querySelect('#send');
+const $buttonMailto = document.querySelector('#send');
 
 
-$form.addEventListener("submit, envioMail");
+$form.addEventListener("submit", envioMail);
 
 function envioMail(event) {
     event.preventDefault();
     const form = new FormData(this);
-    $buttonMailto.setAttribute("href", 'mailto:ayelenleclerc@gmail.com?subject=${form.get("nombre") ${form.get("email")}}&body=${form.get("mensaje")}')
+    console.log(form.get("nombre"));
+    $buttonMailto.setAttribute("href", 'mailto:ayelenleclerc@gmail.com? subject=${form.get("nombre") ${form.get("email")}}&body=${form.get("mensaje")}');
     $buttonMailto.click();
 
 }
