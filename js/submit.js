56958b5007350905f18e53a646a07cab72d6853e
ayelenleclerc <ayelenleclerc@gmail.com>
2021-11-22 17:43:48 -0300

    terminado con el envio de mail completo

--- js/submit.js
+++ js/submit.js
@@ -1,14 +1,20 @@
 const $form = document.querySelector('#form');
-const $buttonMailto = document.querySelector('#send');
-
 
 $form.addEventListener("submit", envioMail);
 
-function envioMail(event) {
+async function envioMail(event) {
     event.preventDefault();
     const form = new FormData(this);
-    console.log(form.get("nombre"));
-    $buttonMailto.setAttribute("href", 'mailto:ayelenleclerc@gmail.com? subject=${form.get("nombre") ${form.get("email")}}&body=${form.get("mensaje")}');
-    $buttonMailto.click();
+   const response = await fetch(this.action, {
+        method: this.method,
+        body: form,
+        headers: {
+            'Accept': 'application/json'
+        }
+   })
+    if (response.ok) {
+        this.reset();
+        alert('¡Gracias por contactarnos!')
+    }
 
 }
